0e24e0e08c82877dbadd72f4fd127221ccf5beae
fonypage <misha.ignatenko.2017@bk.ru>
2023-11-26 21:05:44 +0300

    add new Div

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div>xyi</div>
-      <div>ptdfyhfhgf</div>
+      <div>pdkjhgfdszfhfgdrsdfghf</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
