94d3a14e77d3f1694a263a308002a0925ceca879
fonypage <misha.ignatenko.2017@bk.ru>
2023-11-26 20:51:06 +0300

    changePenis

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>sdvuijasiodjfiojas</div>
+      <div>xyi</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
