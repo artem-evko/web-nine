4b71d5d2b3a1f195c6543a553931ad198f718b44
klernkmale <kiarealmaslow@gmail.com>
2023-12-06 22:14:58 +0300

    add a hero component to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import Hero from './components/hero';
 
 function App() {
   return (
-   <Header/>
+    <div>
+      <Header/>
+      <Hero/>
+    </div>
+   
+   
   );
 }
 
